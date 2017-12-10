4d2a1533a7fc767f87d06b142ee77ebc67aa8001
Per Nyberg <per_skuru@msn.com>
2017-12-10 22:37:09 +0100

    Added handling of unnecessary parenthesises

--- countdown_calculator.py
+++ countdown_calculator.py
@@ -1,42 +1,43 @@
-# main method
-#  currently takes a list of numbers and a target and prints the info about the calculation
+'''
+	main method
+	currently takes a list of numbers and a target and prints the info about the calculation
+'''
 def solve_for_target(numbers, target):
 	max_number_of_parenthesis = len(numbers) - 1
 	result = naive_check_for_additative_solution(numbers, target, [], 0, max_number_of_parenthesis)
+
+	result = remove_unnecessary_parenthesises(result)
 
 	print(numbers)
 	print(target)
 	print(result)
 
-def simple_check_for_additative_or_subtractive_solution(usable_list, target, sum, used_list):
-	if sum == target:
-		return used_list
+def remove_unnecessary_parenthesises(result):
+	starting_index = -1
+	stopping_index = -1
 
-	if len(usable_list) == 0:
-		return []
+	index = 0
+	while index < len(result) - 1:
+		if result[index] == '(':
+			starting_index = index
+		elif result[index] == ')':
+			if result[starting_index-1] == '(' and result[index+1] == ')': 
+				result = result[0:starting_index] + result[starting_index+1:index] + result[index+1:len(result)]
+				starting_index = -1
+				stopping_index = -1
+				index = -1
 
-	active_number = usable_list[0]
+		index += 1
 
-	# check with added number
-	result_with_added_number = simple_check_for_additative_or_subtractive_solution(usable_list[1:], target, sum+active_number, used_list + ["+"] + [active_number])
-
-	if result_with_added_number != []:
-		return result_with_added_number
-
-	# check with substracted number
-
-	result_with_subtracted_number = simple_check_for_additative_or_subtractive_solution(usable_list[1:], target, sum-active_number, used_list + ["-"] + [active_number])
-
-	if result_with_subtracted_number != []:
-		return result_with_subtracted_number
-
-	# default case (does nothing)
-	result_without_change = simple_check_for_additative_or_subtractive_solution(usable_list[1:], target, sum, used_list)
-
-	return result_without_change
+	return result
 
 def naive_check_for_additative_solution(usable_list, target, used_list, open_parenthesis, number_of_allowed_parenthesis_left):
 	# calculates the value of the 
+	if len(used_list) > 4 and used_list[0] == '(' and used_list[2] == '+' and used_list[4] == ')' and \
+			(used_list[1] == 3 and used_list[3] == 5 or used_list[1] == 5 and used_list[3] == 3) and \
+			len(usable_list) > 0:
+		print(used_list)
+		print("Usable list: " + str(usable_list))
 	sum = calculate_sum(used_list)
 
 	if sum == target:
@@ -46,7 +47,7 @@ def naive_check_for_additative_solution(usable_list, target, used_list, open_par
 		active_number = usable_list[i]
 		copied_usable_list = usable_list[0:i] + usable_list[i+1:len(usable_list)]
 
-		if has_last_element(used_list) and last_element_was_digit(used_list):
+		if has_last_element(used_list) and (last_element_was_digit(used_list) or last_element_was_ending_parenthesis(used_list)):
 			#print(str(used_list[-1]) + " ----- adding/subtracting/multiplying/dividing ----- " + str(used_list))
 
 			# check when adding
@@ -80,7 +81,7 @@ def naive_check_for_additative_solution(usable_list, target, used_list, open_par
 			if res_with_number != []:
 				return res_with_number
 
-		if has_last_element(used_list) and last_element_was_digit(used_list) or last_element_was_ending_parenthesis(used_list):
+		if has_last_element(used_list) and (last_element_was_digit(used_list) or last_element_was_ending_parenthesis(used_list)):
 			#print(str(used_list[-1]) + " ----- end-parenthesis ----- " + str(used_list))
 
 			# check when ending parenthesis
@@ -130,7 +131,8 @@ def calculate_sum(expression_list):
 def calculate_sum_helper(start_index, less_than_bound, expression_list):
 	active_parenthesises = 0
 	# first solve parenthesises
-	for index in range(start_index, less_than_bound):
+	index = start_index
+	while index < less_than_bound:
 		if expression_list[index] == '(':
 			if active_parenthesises == 0:
 				new_start_index = index+1
@@ -138,11 +140,13 @@ def calculate_sum_helper(start_index, less_than_bound, expression_list):
 		elif expression_list[index] == ')':
 			active_parenthesises -= 1 	# no need to check < 0, because it cannot be
 			if active_parenthesises == 0:
-				parenthesis_sum = calculate_sum_helper(start_index, index, expression_list)
+				parenthesis_sum = calculate_sum_helper(start_index+1, index, expression_list)
 				expression_list_length = len(expression_list)
-				expression_list = expression_list[0:start_index-1] + [parenthesis_sum] + expression_list[index+1:less_than_bound]
+				expression_list = expression_list[0:start_index] + [parenthesis_sum] + expression_list[index+1:len(expression_list)]
 				less_than_bound = less_than_bound - expression_list_length + len(expression_list) # recalculating the less_than_bound
 				index = index - expression_list_length + len(expression_list)
+
+		index += 1
 
 	# secondly, solve multiplikation and division
 	index = start_index
@@ -155,7 +159,7 @@ def calculate_sum_helper(start_index, less_than_bound, expression_list):
 			if not str(first_number).isdigit() or not str(second_number).isdigit():
 				return None
 			product = first_number * second_number
-			expression_list = expression_list[0:index-1] + [product] + expression_list[index+2:less_than_bound]
+			expression_list = expression_list[0:index-1] + [product] + expression_list[index+2:len(expression_list)]
 			index -= 1
 			less_than_bound -= 2
 		elif expression_list[index] == '/':
@@ -166,7 +170,7 @@ def calculate_sum_helper(start_index, less_than_bound, expression_list):
 			if not str(first_number).isdigit() or not str(second_number).isdigit():
 				return None
 			quotient = first_number * second_number
-			expression_list = expression_list[0:index-1] + [quotient] + expression_list[index+2:less_than_bound]
+			expression_list = expression_list[0:index-1] + [quotient] + expression_list[index+2:len(expression_list)]
 			index -= 1
 			less_than_bound -= 2
 
@@ -183,7 +187,7 @@ def calculate_sum_helper(start_index, less_than_bound, expression_list):
 			if not str(first_number).isdigit() or not str(second_number).isdigit():
 				return None
 			sum_value = first_number + second_number
-			expression_list = expression_list[0:index-1] + [sum_value] + expression_list[index+2:less_than_bound]
+			expression_list = expression_list[0:index-1] + [sum_value] + expression_list[index+2:len(expression_list)]
 			index -= 1
 			less_than_bound -= 2
 		elif expression_list[index] == '-':
@@ -194,7 +198,7 @@ def calculate_sum_helper(start_index, less_than_bound, expression_list):
 			if not str(first_number).isdigit() or not str(second_number).isdigit():
 				return None
 			difference = first_number - second_number
-			expression_list = expression_list[0:index-1] + [difference] + expression_list[index+2:less_than_bound]
+			expression_list = expression_list[0:index-1] + [difference] + expression_list[index+2:len(expression_list)]
 			index -= 1
 			less_than_bound -= 2
 
@@ -202,9 +206,9 @@ def calculate_sum_helper(start_index, less_than_bound, expression_list):
 
 	if len(expression_list) == 0:
 		return None
-	return expression_list[0]
+	return expression_list[start_index]
 
-numbers = [3, 5, 9, 7]
-target = 128
+numbers = [3, 5, 7]
+target = 14
 
 solve_for_target(numbers, target)
